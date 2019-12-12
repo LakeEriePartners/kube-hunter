dd1112d2ec46ddff7aaf3616492285506c24cfe7
Jason Rojas <jason.rojas60037@gmail.com>
2019-12-12 13:57:15 -0500

    Add basic auth dispatch.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM python:3.7-alpine3.10 as builder
 
-RUN apk add --no-cache \
+RUN apk add --update \
     linux-headers \
     tcpdump \
     build-base \
     ebtables
 
-WORKDIR /kube-hunter
+RUN mkdir -p /kube-hunter 
 COPY ./requirements.txt /kube-hunter/.
 RUN pip install -r /kube-hunter/requirements.txt -t /kube-hunter
 
@@ -14,12 +14,10 @@ COPY . /kube-hunter
 
 FROM python:3.7-alpine3.10
 
-RUN apk add --no-cache \
+RUN apk add --update \
     tcpdump
-RUN apk upgrade --no-cache 
+RUN apk upgrade 
 
 COPY --from=builder /kube-hunter /kube-hunter
 
 WORKDIR /kube-hunter
-
-ENTRYPOINT ["python",  "kube-hunter.py"]
--- src/modules/report/dispatchers.py
+++ src/modules/report/dispatchers.py
@@ -7,23 +7,34 @@ from __main__ import config
 class HTTPDispatcher(object):
     def dispatch(self, report):
         logging.debug('Dispatching report via http')
-        dispatch_method = os.environ.get(
+        dispatchMethod = os.environ.get(
             'KUBEHUNTER_HTTP_DISPATCH_METHOD',
             'POST'
         ).upper()
-        dispatch_url = os.environ.get(
+        dispatchURL = os.environ.get(
             'KUBEHUNTER_HTTP_DISPATCH_URL',
             'https://localhost/'
         )
+        dispatchAUTHUSERNAME = os.environ.get(
+            'KUBEHUNTER_HTTP_DISPATCH_USERNAME',
+            ''
+        )
+        dispatchAUTHPASSWORD = os.environ.get(
+            'KUBEHUNTER_HTTP_DISPATCH_PASSWORD',
+            ''
+        )
         try:
             r = requests.request(
-                dispatch_method,
-                dispatch_url,
+                dispatchMethod,
+                dispatchURL,
                 json=report,
-                headers={'Content-Type': 'application/json'}
+                headers={'Content-Type': 'application/json'},
+                auth=(dispatchAUTHUSERNAME, dispatchAUTHPASSWORD)
             )
             r.raise_for_status()
-            logging.info('\nReport was dispatched to: {url}'.format(url=dispatch_url))
+            logging.info('\nReport was dispatched to: {url}'.format(url=dispatchURL))
+            if dispatchAUTHUSERNAME:
+                logging.info('\nWe used password and username on this request')
             logging.debug(
                 "\tResponse Code: {status}\n\tResponse Data:\n{data}".format(
                     status=r.status_code,
@@ -33,17 +44,19 @@ class HTTPDispatcher(object):
         except requests.HTTPError as e:
             # specific http exceptions
             logging.error(
-                "\nCould not dispatch report using HTTP {method} to {url}\nResponse Code: {status}".format(
+                "\nCould not dispatch report using HTTP {method} to {url} with auth ({username}:{password})\nResponse Code: {status}".format(
                     status=r.status_code,
-                    url=dispatch_url,
-                    method=dispatch_method
+                    url=dispatchURL,
+                    method=dispatchMethod,
+                    username=dispatchAUTHUSERNAME,
+                    password=dispatchAUTHPASSWORD
                 )
             )
         except Exception as e:
             # default all exceptions
             logging.error("\nCould not dispatch report using HTTP {method} to {url} - {error}".format(
-                method=dispatch_method,
-                url=dispatch_url,
+                method=dispatchMethod,
+                url=dispatchURL,
                 error=e
             ))
 
